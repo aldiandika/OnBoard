a841b1c630325dda640a579a10cc5b48389647c6
aldiandika <aldi.andika@gmail.com>
2017-05-03 11:27:11 +0700

    Fixed Tab

--- app/src/main/java/com/example/aldiandika/onboard/MainActivity.java
+++ app/src/main/java/com/example/aldiandika/onboard/MainActivity.java
@@ -9,7 +9,10 @@ import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.HorizontalScrollView;
 import android.widget.ProgressBar;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 
 public class MainActivity extends FragmentActivity {
 
@@ -27,6 +30,7 @@ public class MainActivity extends FragmentActivity {
         progAtas = (ProgressBar) findViewById(R.id.progAtas);
         progAtas.getProgressDrawable().setColorFilter(
                 Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+        //        progAtas.setProgressTintList(ColorStateList.valueOf(Color.RED));
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         tabhost = (FragmentTabHost) findViewById(R.id.tabhost);
         tabhost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
@@ -42,17 +46,65 @@ public class MainActivity extends FragmentActivity {
         pager = (ViewPager) findViewById(R.id.pager);
         setupViewPager(pager);
 
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
 
-//        progAtas.setProgressTintList(ColorStateList.valueOf(Color.RED));
+            @Override
+            public void onPageSelected(int position) {
+                tabhost.setCurrentTab(position);
+            }
 
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        tabhost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                pager.setCurrentItem(tabhost.getCurrentTab());
+//                scrollToCurrentTab();
+            }
+        });
 
     }
 
-    private void setupViewPager(ViewPager viewPager){
+
+
+    private void setupViewPager(ViewPager pager){
         SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
         adapter.addFragment(new HomeFragment(),tabhost.getCurrentTabTag());
         adapter.addFragment(new TimeFragment(),tabhost.getCurrentTabTag());
         pager.setAdapter(adapter);
     }
+
+
 }
+
+//    private void initializeHorizontalTabs() {
+//        LinearLayout ll = (LinearLayout) tabWidget.getParent();
+//        horizontalScrollView = new HorizontalScrollView(this);
+//        horizontalScrollView.setLayoutParams(new FrameLayout.LayoutParams(
+//                FrameLayout.LayoutParams.MATCH_PARENT,
+//                FrameLayout.LayoutParams.WRAP_CONTENT));
+//        ll.addView(horizontalScrollView, 0);
+//        ll.removeView(tabWidget);
+//        horizontalScrollView.addView(tabWidget);
+//        horizontalScrollView.setHorizontalScrollBarEnabled(false);
+//    }
+
+//    private void scrollToCurrentTab() {
+//        final int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+//        final int leftX = tabs.getChildAt(tabhost.getCurrentTab()).getLeft();
+//        int newX = 0;
+//
+//        newX = leftX + (tabs.getChildAt(tabhost.getCurrentTab()).getWidth() / 2) - (screenWidth / 2);
+//        if (newX < 0) {
+//            newX = 0;
+//        }
+//        horizontalScrollView.scrollTo(newX, 0);
+//    }
