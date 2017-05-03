c61e59733c5e7ca729fa800a2fd97ac2ee5fdcfe
aldiandika <aldi.andika@gmail.com>
2017-05-03 09:27:58 +0700

    Layout Atas Kasar

--- app/src/main/java/com/example/aldiandika/onboard/MainActivity.java
+++ app/src/main/java/com/example/aldiandika/onboard/MainActivity.java
@@ -4,13 +4,16 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     ProgressBar progAtas;
+    FragmentTabHost tabhost;
 
 //    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -19,10 +22,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         progAtas = (ProgressBar) findViewById(R.id.progAtas);
+        progAtas.getProgressDrawable().setColorFilter(
+                Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+
+        tabhost = (FragmentTabHost) findViewById(R.id.tabhost);
+        tabhost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        tabhost.addTab(
+                tabhost.newTabSpec("home").setIndicator("Home", null),
+                HomeFragment.class, null);
+        tabhost.addTab(
+                tabhost.newTabSpec("Timeline").setIndicator("Timeline", null),
+                TimeFragment.class, null);
 
 //        progAtas.setProgressTintList(ColorStateList.valueOf(Color.RED));
 
-        progAtas.getProgressDrawable().setColorFilter(
-                Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+
     }
 }
---
+++ app/src/main/java/com/example/aldiandika/onboard/TimeFragment.java
@@ -0,0 +1,22 @@
+package com.example.aldiandika.onboard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Crew on 5/3/2017.
+ */
+
+public class TimeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.timeline_fragment, container, false);
+        return v;
+    }
+}
