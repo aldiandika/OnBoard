a6ba436aea5253abee047aeafd36abbbb5b30aff
aldiandika <aldi.andika@gmail.com>
2017-05-03 08:31:27 +0700

    Layout Atas Kasar

--- app/src/main/java/com/example/aldiandika/onboard/MainActivity.java
+++ app/src/main/java/com/example/aldiandika/onboard/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.aldiandika.onboard;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar progAtas;
+
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progAtas = (ProgressBar) findViewById(R.id.progAtas);
+
+//        progAtas.setProgressTintList(ColorStateList.valueOf(Color.RED));
+
+        progAtas.getProgressDrawable().setColorFilter(
+                Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
     }
 }
